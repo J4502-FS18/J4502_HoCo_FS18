ad84465d44aaeb3505fa2d16d737f1f27926a1b9
zsayer <zacharysayer@mail.missouri.edu>
2018-11-11 16:17:21 -0600

    footer fixed and hamburger menu

--- menu.js
+++ menu.js
@@ -1,12 +1,12 @@
 $(document).ready(function(){
   $('.menu-trigger').click(function(){
-      $('nav ul').slideToggle(500);
+      $('.page').slideToggle(500);
       }); //(closing slidetoggle)
 
 
 $(window).resize(function(){
   if ($(window).width()>500){
-    $('nav ul').removeAttr('style');
+    $('.page').removeAttr('style');
 }
 
 }) //(closing resize function)
